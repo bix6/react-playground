1a459550c79adb1faad3eda4606936ea30890ec4
bix <840573+bix6@users.noreply.github.com>
2019-12-13 11:27:47 -0800

    add HelloWorld

---
+++ src/state-drills/HelloWorld.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+    state = {
+        who: 'World'
+    }
+    handleWorld = () => {
+        this.setState({who: 'World'});
+    }
+    handleFriend = () => {
+        this.setState({who: 'Friend'})
+    }
+    handleReact = () => {
+        this.setState({who: 'React'})
+    }
+    render() {
+        return (
+            <div>
+                <p>Hello, {this.state.who}</p>
+                <button onClick={this.handleWorld}>
+                    World
+                </button>
+                <button onClick={this.handleFriend}>
+                    Friend
+                </button>
+                <button onClick={this.handleReact}>
+                    React
+                </button>
+            </div>
+        )
+    }
+}
+
+export default HelloWorld;
