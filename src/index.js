42d86c79efb9c81cfa2cb09f5574cbffcd908db7
bix <840573+bix6@users.noreply.github.com>
2020-01-07 11:18:38 -0800

    add complete demonymapp

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
 // import HelloWorld from './state-drills/HelloWorld';
 // import Bomb from './state-drills/Bomb';
 // import RouletteGun from './state-drills/RouletteGun';
+import DemonymApp from './demonymapp/demonymApp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(<HelloWorld />, document.getElementById('root'));
 // ReactDOM.render(<Bomb />, document.getElementById('root'));
 // ReactDOM.render(<RouletteGun bulletInChamber={8} />, document.getElementById('root'));
+ReactDOM.render(<DemonymApp />, document.getElementById('root'));
