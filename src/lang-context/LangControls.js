44a4d5a3b410d645a99a994c34d920da31f71081
bix <840573+bix6@users.noreply.github.com>
2020-01-13 10:22:50 -0800

    contextValue question

--- src/lang-context/LangControls.js
+++ src/lang-context/LangControls.js
@@ -5,21 +5,26 @@ export default function LangControls(props) {
     return (
         <LanguageContext.Consumer>
             {(value) => {
+                console.log(value);
+                console.log(value.contextValue);
                 return (
                     <>
                         <button
+                            onClick={() => props.onSetLang('en-GB')}
                             disabled={value.lang === 'en-GB'}>
                             British{' '}
                             <span role="img" aria-label="en-GB">GB</span>
                         </button>
                         {' '}
                         <button
-                            disabled={value.lang === 'en-US'}>
+                            onClick={() => props.onSetLang('en-US')}
+                            disabled={value.contextValue.lang === 'en-US'}>
                             American{' '}
                             <span role="img" aria-label="en-US">US</span>
                         </button>
                         {' '}
                         <button
+                            onClick={() => props.onSetLang('ko')}
                             disabled={value.lang === 'ko'}>
                             Korean{' '}
                             <span role="img" aria-label="ko">KR</span>
